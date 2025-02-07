ce7042e8f41b703640d177f94a33741308d31828
OlenaBogoliubova <143827030+OlenaBogoliubova@users.noreply.github.com>
2025-02-07 09:22:02 +0100

    Update landing_to_bronze.py

--- landing_to_bronze.py
+++ landing_to_bronze.py
@@ -1,15 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 from pyspark.sql import SparkSession
 import requests
 
 
 def download_data(file):
-    """Download data from the server and save it locally as a CSV file."""
+    """Завантажуємо дані"""
     url = f"https://ftp.goit.study/neoversity/{file}.csv"
     local_path = f"{file}.csv"
 
@@ -25,16 +19,14 @@ def download_data(file):
 
 
 def process_table(spark, table):
-    """Process a single table: read CSV, convert to Parquet, and save."""
+    """Обробка таблиці CSV, конвертація у Parquet, збереження."""
     local_path = f"{table}.csv"
     output_path = f"/tmp/bronze/{table}"
 
     print("Processing {table}...")
 
-    # Read the CSV into a Spark DataFrame
     df = spark.read.csv(local_path, header=True, inferSchema=True)
 
-    # Write the DataFrame to Parquet
     df.write.mode("overwrite").parquet(output_path)
     print(f"Data saved to {output_path}")
 
@@ -43,8 +35,7 @@ def process_table(spark, table):
 
 
 def landing_to_bronze():
-    """Main flow for processing tables from landing to bronze."""
-    tables = ["athlete_bio", "athlete_event_results"]
+   tables = ["athlete_bio", "athlete_event_results"]
 
     # Create a single Spark session
     spark = SparkSession.builder.appName("LandingToBronze").getOrCreate()
